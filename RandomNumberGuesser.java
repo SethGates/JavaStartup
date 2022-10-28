777e41ef934cdc8ef0b7d3a7be291b875e41be64
SethGates <1sethgates@gmail.com>
2022-10-28 11:36:09 -0700

    Added guess counter.

--- RandomNumberGuesser.java
+++ RandomNumberGuesser.java
@@ -8,22 +8,25 @@ public class RandomNumberGuesser {
         int randomValue = 1 + (int) (100 * Math.random()); // Selects through 1-100. Change value to whatever youd like.
 
         System.out.println("Guess a number between 1 - 100"); // Ask user for input
+        int numberOfGuesses = 0; // Sets baseline for number of guesses so it can increase at every attempt.
         int userValue = input.nextInt(); // Establish "input"
-        while (userValue != randomValue) { //Evaluator for accuracy for H or L
+
+        while (userValue != randomValue) { // Evaluator for accuracy for H or L
             if (userValue < randomValue) {
                 System.out.println("Too low!");
-            }
-            else {
+            } else {
                 System.out.println("Too high!");
             }
-            System.out.println("Guess a number between 1 - 100"); //Ask question again after incorrect input
+            numberOfGuesses ++;
+            System.out.println("Guess a number between 1 - 100"); // Ask question again after incorrect input
             userValue = input.nextInt();
         }
         System.out.println("You won! " + randomValue);
+        System.out.println("Your total guesses " + numberOfGuesses);
+        
 
         input.close();
 
     }
 
 }
-
