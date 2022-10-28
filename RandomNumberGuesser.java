bcd22f7952a00930a48c592e9b9fcf4a956b82ac
SethGates <1sethgates@gmail.com>
2022-10-28 11:23:17 -0700

    Random Number Generator, first small 'project' in Java.

---
+++ RandomNumberGuesser.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class RandomNumberGuesser {
+    public static void main(String[] args) {
+        // establish "input"
+        Scanner input = new Scanner(System.in);
+
+        int randomValue = 1 + (int) (100 * Math.random()); // Selects through 1-100. Change value to whatever youd like.
+
+        System.out.println("Guess a number between 1 - 100"); // Ask user for input
+        int userValue = input.nextInt(); // Establish "input"
+        while (userValue != randomValue) { //Evaluator for accuracy for H or L
+            if (userValue < randomValue) {
+                System.out.println("Too low!");
+            }
+            else {
+                System.out.println("Too high!");
+            }
+            System.out.println("Guess a number between 1 - 100"); //Ask question again after incorrect input
+            userValue = input.nextInt();
+        }
+        System.out.println("You won! " + randomValue);
+
+        input.close();
+
+    }
+
+}
+
